732bd3d9586419df8f63426d26c4326735617b40
skwdhc <133957547+skwdhc@users.noreply.github.com>
2024-04-16 17:02:30 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   appid: "wx6a9bbb3effcd585b",
   appsecret: "9b4e31ad3d7574dd4c0b947a5f319ce4",
-  template_id: "6mjwCeMdlLJt5Kd3QY517BISqW5F4AUqB0b4r9GonTw",
+  template_id: "RAX4RL7l-m4qae4ogQMoqH3UifjAEFA0sKziwgm2ZDk",
   touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
